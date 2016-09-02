6c1a2e09b842febcaf169553e24b55084da6891f
shadow <chak.melody@gmail.com>
2016-09-02 09:58:47 +0800

    change jsonObject

--- manifold-base/src/main/java/com/lanyine/manifold/base/beans/JsonObject.java
+++ manifold-base/src/main/java/com/lanyine/manifold/base/beans/JsonObject.java
@@ -6,79 +6,81 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.Serializable;
 
 /**
- * JSON类
+ * JSON
  *
  * @author shadow
  */
 public final class JsonObject implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 3914211970422213626L;
 
-    /**
-     * 操作响应码
-     **/
-    private String code = "0";
+	/**
+	 * The response code for operation
+	 **/
+	private String code = "0";
 
-    /**
-     * 返回的消息，暂时留用
-     **/
-    private String msg; // 返回的消息
+	/**
+	 * message
+	 **/
+	private String msg;
 
-    /**
-     * 需要处理的数据对象
-     **/
-    private Object data;
+	/**
+	 * data object
+	 **/
+	private Object data;
 
-    /**
-     * 分页概述
-     **/
-    private Pagination pagination;
+	/**
+	 * The paging information
+	 **/
+	private Pagination page;
 
-    public String getCode() {
-        return code;
-    }
+	public String getCode() {
+		return code;
+	}
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+	public JsonObject setCode(String code) {
+		this.code = code;
+		return this;
+	}
 
-    public Object getData() {
-        return data;
-    }
+	public Object getData() {
+		return data;
+	}
 
-    public JsonObject setData(Object data) {
-        // 如果数据是分页信息，则自动设置分页信息
-        if (data instanceof PageResponse) {
-            this.setPagination(((PageResponse<?>) data).toPagination());
-        }
-        this.data = data;
-        return this;
-    }
+	public JsonObject setData(Object data) {
+		// If the data instance of PageResponse, set the paging
+		if (data instanceof PageResponse) {
+			PageResponse<?> rdata = (PageResponse<?>) data;
+			this.setPage(rdata.toPagination());
+		}
+		this.data = data;
+		return this;
+	}
 
-    public String getMsg() {
-        return msg;
-    }
+	public String getMsg() {
+		return msg;
+	}
 
-    public JsonObject setMsg(String msg) {
-        this.msg = msg;
-        return this;
-    }
+	public JsonObject setMsg(String msg) {
+		this.msg = msg;
+		return this;
+	}
 
-    public Pagination getPagination() {
-        return pagination;
-    }
+	public Pagination getPage() {
+		return page;
+	}
 
-    public JsonObject setPagination(Pagination pagination) {
-        this.pagination = pagination;
-        return this;
-    }
+	public JsonObject setPage(Pagination pagination) {
+		this.page = pagination;
+		return this;
+	}
 
-    @Override
-    public String toString() {
-        try {
-            return new ObjectMapper().writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            return null;
-        }
-    }
-
+	@Override
+	public String toString() {
+		try {
+			return new ObjectMapper().writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
